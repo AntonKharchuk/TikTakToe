7f4479ba5e120ed0f1caae29a831b7cd7c633310
Anton Kharchuk <Kharchuk.anton@gmail.com>
2024-01-14 08:51:16 +0200

    Add DI

---
+++ TikTakToe.ConsoleDemo/DI.cs
@@ -0,0 +1,22 @@
+﻿
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TikTakToe.ConsoleDemo
+{
+    public static class DI
+    {
+        public static IServiceCollection ConfigureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            // Add the implementation types and specify the interfaces
+           return  services.AddSingleton(configuration)
+                    .AddScoped<IPrintGame, ConsoleGamePrinterWithArrowSelection>()
+                    .AddScoped<IApiClient>(provider =>
+                    {
+                        var baseAddress = configuration["ApiSettings:LocalHostAddress"];
+                        return new ApiClient(baseAddress);
+                    })
+                    .AddScoped<Game, Game>();
+        }
+    }
+}
--- TikTakToe.ConsoleDemo/Program.cs
+++ TikTakToe.ConsoleDemo/Program.cs
@@ -1,12 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
 using TikTakToe;
+using TikTakToe.ConsoleDemo;
 
-var consoleGamePrinter = new ConsoleGamePrinterWithArrowSelection();
+//var consoleGamePrinter = new ConsoleGamePrinterWithArrowSelection();
 
-string baseAddress = "https://188c-31-40-110-212.ngrok-free.app/";
+//string baseAddress = "https://188c-31-40-110-212.ngrok-free.app/";
 
-var apiClient = new ApiClient(baseAddress);
+//var apiClient = new ApiClient(baseAddress);
 
-var game = new Game(consoleGamePrinter, apiClient);
+//var game = new Game(consoleGamePrinter, apiClient);
 
-await game.Run();
+//await game.Run();
+
+
+
+IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+// Set up Dependency Injection
+var serviceProvider = new ServiceCollection()
+    .ConfigureServices(configuration)
+    .BuildServiceProvider();
+
+// Run the application
+var game = serviceProvider.GetRequiredService<Game>();
+game.Run().GetAwaiter().GetResult();
